12f460466df7d722176d63b8494f9f04b71c221a
sleipnir22 <bolovindenis@gmail.com>
2021-12-14 14:54:58 +0700

    Добавил класс CRUD_project, в котором создал статический метод Create, вызывающийся из контролле ProjectsController

---
+++ FamilyBudget/CRUD models/CRUD_project.cs
@@ -0,0 +1,77 @@
+﻿using FamilyBudget.Controllers;
+using FamilyBudget.Data;
+using FamilyBudget.Models;
+using FamilyBudget.Extensions;
+using Microsoft.AspNetCore.Identity;
+using System.Collections.Generic;
+using System;
+
+namespace FamilyBudget.CRUD_models
+{
+    public class CRUD_project
+    {
+        public static void Create(Project project, IdentityUser user, ApplicationDbContext context)
+        {
+            var timeNow = DateTime.Now;
+            project.ProjectMembers.Add(new ProjectMember
+            {
+                    NameInProject = "Владелец",
+                    User = user,
+                    UserId = user.Id,
+                    ProjectId = project.Id,
+                    Project = project,
+                    CreateTime = timeNow,
+                    UpdateTime = timeNow
+            });
+
+            project.Categories.Add(new Category 
+            {  
+                ProjectId = project.Id, 
+                Name = "Продукты", 
+                Project = project, 
+                CreateTime = timeNow, 
+                UpdateTime = timeNow 
+            });
+            project.Categories.Add(new Category 
+            { 
+                ProjectId = project.Id, 
+                Name = "Транспорт", 
+                Project = project, 
+                CreateTime = timeNow, 
+                UpdateTime = timeNow 
+            });
+            project.Categories.Add(new Category
+            {
+                ProjectId = project.Id,
+                Name = "Кафе",
+                Project = project,
+                CreateTime = timeNow,
+                UpdateTime = timeNow
+            });
+            project.Categories.Add(new Category
+            {
+                ProjectId = project.Id,
+                Name = "Развлечения",
+                Project = project,
+                CreateTime = timeNow,
+                UpdateTime = timeNow
+            });
+            project.Categories.Add(new Category
+            {
+                ProjectId = project.Id,
+                Name = "Подарки",
+                Project = project,
+                CreateTime = timeNow,
+                UpdateTime = timeNow
+            });
+            project.Categories.Add(new Category
+            {
+                ProjectId = project.Id,
+                Name = "Бытовая техника",
+                Project = project,
+                CreateTime = timeNow,
+                UpdateTime = timeNow
+            });
+        }
+    }
+}

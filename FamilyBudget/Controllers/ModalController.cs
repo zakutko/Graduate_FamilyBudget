3f5412ee970c5c703ee44087809cf7cc55ee1ffd
e543 <georgy_lisunov@mail.ru>
2021-12-16 16:58:04 +0700

    Добавлен Modal контроллер с представлениями. Изменены навзвания. Удален Model в представлениях Home

---
+++ FamilyBudget/Controllers/ModalController.cs
@@ -0,0 +1,47 @@
+﻿using FamilyBudget.Data;
+using FamilyBudget.Extensions;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace FamilyBudget.Controllers
+{
+    public class ModalController : Controller
+    {
+
+        private readonly ApplicationDbContext _context;
+
+        private IdentityUser user { get { return CurrentUser(); } }
+        public ModalController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        public IActionResult ProjectDelete(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var project = _context.Projects.FirstOrDefault(p => p.Id == id);
+
+            if (project == null)
+            {
+                return NotFound();
+            }
+
+            return PartialView(project);
+        }
+
+        private IdentityUser CurrentUser()
+        {
+            var username = HttpContext.User.Identity.Name;
+            return _context.Users
+                .FirstOrDefault(m => m.UserName == username);
+        }
+    }
+}

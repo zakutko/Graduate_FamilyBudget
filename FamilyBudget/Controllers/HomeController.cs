c88bf77c76532364e71c686f9299904965b359d1
e543 <georgy_lisunov@mail.ru>
2022-01-12 21:46:41 +0700

    плюсы

--- FamilyBudget/Controllers/HomeController.cs
+++ FamilyBudget/Controllers/HomeController.cs
@@ -113,7 +113,7 @@ namespace FamilyBudget.Controllers
                     break;
                 case FinOperationSortModelEnum.FinType:
                     if (sortDir == FinOperationSortDirEnum.Ascending) { finOperations = finOperations.OrderBy(s => s.FinType); break; }
-                    
+
                     finOperations = finOperations.OrderByDescending(s => s.FinType);
                     break;
                 case FinOperationSortModelEnum.Category:
@@ -185,6 +185,14 @@ namespace FamilyBudget.Controllers
 
 
             var editModel = new HomeProjectEditModel();
+
+            var projectMembers = _context.ProjectMembers.Where(x => x.ProjectId == project.Id);
+            var categories = _context.Categories.Where(x => x.ProjectId == project.Id);
+
+            editModel.projectId = project.Id;
+            editModel.project = project;
+            editModel.projectMembers = await projectMembers.ToListAsync();
+            editModel.categories = await categories.ToListAsync();
             return View(editModel);
         }
 
--- FamilyBudget/Models/View/Edit/HomeProjectEditModel.cs
+++ FamilyBudget/Models/View/Edit/HomeProjectEditModel.cs
@@ -1,6 +1,12 @@
-﻿namespace FamilyBudget.Models.View.Edit
+﻿using System.Collections.Generic;
+
+namespace FamilyBudget.Models.View.Edit
 {
     public class HomeProjectEditModel
     {
+        public int projectId { get; set; }
+        public Project project { get; set; }
+        public List<ProjectMember> projectMembers{ get; set; }
+        public List<Category> categories { get; set; }
     }
 }

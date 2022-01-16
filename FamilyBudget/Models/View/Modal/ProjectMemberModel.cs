7c3002d5733167d7e6e82acf39afc77477bdc757
e543 <georgy_lisunov@mail.ru>
2022-01-17 02:25:41 +0700

    .

--- FamilyBudget/Models/View/Modal/ProjectMemberModel.cs
+++ FamilyBudget/Models/View/Modal/ProjectMemberModel.cs
@@ -1,7 +1,26 @@
-﻿namespace FamilyBudget.Models.View.Modal
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using System;
+
+namespace FamilyBudget.Models.View.Modal
 {
     public class ProjectMemberModel
     {
+        public int Id { get; set; }
 
+        public string NameInProject { get; set; }
+
+        public string UserId { get; set; }
+        public IdentityUser User { get; set; }
+
+        public int ProjectId { get; set; }
+        public Project Project { get; set; }
+
+        public DateTime CreateTime { get; set; }
+        public DateTime UpdateTime { get; set; }
+
+        public bool IsValid { get { return !(IsNotFound || IsForbid); } }
+        public bool IsNotFound { get; set; }
+        public bool IsForbid { get; set; }
     }
 }

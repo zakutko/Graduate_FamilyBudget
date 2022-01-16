a291fa04f9a814c4cd80dacb770018b17886f724
e543 <georgy_lisunov@mail.ru>
2022-01-17 02:25:41 +0700

    .

--- FamilyBudget/Models/View/Modal/CategoryModel.cs
+++ FamilyBudget/Models/View/Modal/CategoryModel.cs
@@ -1,6 +1,23 @@
-﻿namespace FamilyBudget.ModalViewModels
+﻿using FamilyBudget.Models;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using System;
+
+namespace FamilyBudget.ModalViewModels
 {
     public class CategoryModel
     {
+        public int Id { get; set; }
+
+        public string Name { get; set; }
+
+        public int ProjectId { get; set; }
+        public Project Project { get; set; }
+
+        public DateTime CreateTime { get; set; }
+        public DateTime UpdateTime { get; set; }
+
+        public bool IsValid { get { return !(IsNotFound || IsForbid); } }
+        public bool IsNotFound { get; set; }
+        public bool IsForbid { get; set; }
     }
 }

e7f6cf9e68a12ed3a685bae15fffb9c085c96fe9
Алексей <yudakov1814@gmail.com>
2022-01-17 17:32:40 +0700

    Improve tests

--- FamilyBudget.Tests/TestExtensions.cs
+++ FamilyBudget.Tests/TestExtensions.cs
@@ -14,6 +14,9 @@ namespace FamilyBudget.Tests
         IdentityUser User1;
         IdentityUser User2;
         Project Project;
+        Category Category;
+        FinOperation FinOperation;
+        ProjectMember ProjectMember;
 
         [SetUp]
         public void Setup()
@@ -52,85 +55,175 @@ namespace FamilyBudget.Tests
                 Owner = User1,
             };
 
-            Context.Add(User1);
-            Context.Add(User2);
-            Context.Add(Project);
-
-            Context.SaveChanges();
-        }
-
-        [Test]
-        public void TestProjectExtensions()
-        {
-            Assert.True(User1.CanView(Project, Context));
-            Assert.True(User1.CanEdit(Project, Context));
-            Assert.True(User1.CanDelete(Project, Context));
-
-            Assert.False(User2.CanView(Project, Context));
-            Assert.False(User2.CanEdit(Project, Context));
-            Assert.False(User2.CanDelete(Project, Context));
-        }
-
-        [Test]
-        public void TestProjectMemberExtensions()
-        {
-            var projectMember = new ProjectMember
-            {
-                NameInProject = "TestUser",
-                Project = Project,
-            };
-
-            Context.Add(projectMember);
-            Context.SaveChanges();
-
-            Assert.True(User1.CanView(projectMember, Context));
-            Assert.True(User1.CanEdit(projectMember, Context));
-            Assert.True(User1.CanDelete(projectMember, Context));
-
-            Assert.False(User2.CanView(projectMember, Context));
-            Assert.False(User2.CanEdit(projectMember, Context));
-            Assert.False(User2.CanDelete(projectMember, Context));
-        }
-
-        [Test]
-        public void TestCategoryExtensions()
-        {
-            var category = new Category
+            Category = new Category
             {
                 Name = "TestUser",
                 Project = Project,
             };
 
-            Context.Add(category);
-            Context.SaveChanges();
-
-            Assert.True(User1.CanView(category, Context));
-            Assert.True(User1.CanEdit(category, Context));
-            Assert.True(User1.CanDelete(category, Context));
-
-            Assert.False(User2.CanView(category, Context));
-            Assert.False(User2.CanEdit(category, Context));
-            Assert.False(User2.CanDelete(category, Context));
-        }
-
-        [Test]
-        public void TestFinOperationExtensions()
-        {
-            var finOperation = new FinOperation
+            FinOperation = new FinOperation
             {
                 Project = Project,
             };
 
-            Context.Add(finOperation);
+            ProjectMember = new ProjectMember
+            {
+                NameInProject = "TestUser",
+                Project = Project,
+            };
+
+            Context.Add(User1);
+            Context.Add(User2);
+            Context.Add(Project);
+            Context.Add(Category);
+            Context.Add(FinOperation);
+            Context.Add(ProjectMember);
+
             Context.SaveChanges();
+        }
 
-            Assert.True(User1.CanView(finOperation, Context));
-            Assert.True(User1.CanEdit(finOperation, Context));
-            Assert.True(User1.CanDelete(finOperation, Context));
+        [Test]
+        public void TestProjectExtensionsCanViewTrue()
+        {
+            Assert.True(User1.CanView(Project, Context));
+        }
 
-            Assert.False(User2.CanView(finOperation, Context));
-            Assert.False(User2.CanEdit(finOperation, Context));
-            Assert.False(User2.CanDelete(finOperation, Context));
+        [Test]
+        public void TestProjectExtensionsCanViewFalse()
+        {
+            Assert.False(User2.CanView(Project, Context));
+        }
+
+        [Test]
+        public void TestProjectExtensionsCanEditTrue()
+        {
+            Assert.True(User1.CanEdit(Project, Context));
+        }
+
+        [Test]
+        public void TestProjectExtensionsCanEditFalse()
+        {
+            Assert.False(User2.CanEdit(Project, Context));
+        }
+
+        [Test]
+        public void TestProjectExtensionsCanDeleteTrue()
+        {
+            Assert.True(User1.CanDelete(Project, Context));
+        }
+
+        [Test]
+        public void TestProjectExtensionsCanDeleteFalse()
+        {
+            Assert.False(User2.CanDelete(Project, Context));
+        }
+
+        [Test]
+        public void TestProjectMemberExtensionsCanViewTrue()
+        {
+            Assert.True(User1.CanView(ProjectMember, Context));
+        }
+
+        [Test]
+        public void TestProjectMemberExtensionsCanViewFalse()
+        {
+            Assert.False(User2.CanView(ProjectMember, Context));
+        }
+
+        [Test]
+        public void TestProjectMemberExtensionsCanEditTrue()
+        {
+            Assert.True(User1.CanEdit(ProjectMember, Context));
+        }
+
+        [Test]
+        public void TestProjectMemberExtensionsCanEditFalse()
+        {
+            Assert.False(User2.CanEdit(ProjectMember, Context));
+        }
+
+        [Test]
+        public void TestProjectMemberExtensionsCanDeleteTrue()
+        {
+            Assert.True(User1.CanDelete(ProjectMember, Context));
+        }
+
+        [Test]
+        public void TestProjectMemberExtensionsCanDeleteFalse()
+        {
+            Assert.False(User2.CanDelete(ProjectMember, Context));
+        }
+
+        [Test]
+        public void TestCategoryExtensionsCanViewTrue()
+        {
+            Assert.True(User1.CanView(Category, Context));
+        }
+
+        [Test]
+        public void TestCategoryExtensionsCanViewFalse()
+        {
+            Assert.False(User2.CanView(Category, Context));
+        }
+
+        [Test]
+        public void TestCategoryExtensionsCanEditTrue()
+        {
+            Assert.True(User1.CanEdit(Category, Context));
+        }
+
+        [Test]
+        public void TestCategoryExtensionsCanEditFalse()
+        {
+            Assert.False(User2.CanEdit(Category, Context));
+        }
+
+        [Test]
+        public void TestCategoryExtensionsCanDeleteTrue()
+        {
+            Assert.True(User1.CanDelete(Category, Context));
+        }
+
+        [Test]
+        public void TestCategoryExtensionsCanDeleteFalse()
+        {
+            Assert.False(User2.CanDelete(Category, Context));
+        }
+
+        [Test]
+        public void TestFinOperationExtensionsCanViewTrue()
+        {
+            Assert.True(User1.CanView(FinOperation, Context));
+        }
+
+        [Test]
+        public void TestFinOperationExtensionsCanViewFalse()
+        {
+            Assert.False(User2.CanView(FinOperation, Context));
+        }
+
+        [Test]
+        public void TestFinOperationExtensionsCanEditTrue()
+        {
+            Assert.True(User1.CanEdit(FinOperation, Context));
+        }
+
+        [Test]
+        public void TestFinOperationExtensionsCanEditFalse()
+        {
+            Assert.False(User2.CanEdit(FinOperation, Context));
+        }
+
+        [Test]
+        public void TestFinOperationExtensionsCanDeleteTrue()
+        {
+            Assert.True(User1.CanDelete(FinOperation, Context));
+        }
+
+        [Test]
+        public void TestFinOperationExtensionsCanDeleteFalse()
+        {
+            Assert.False(User2.CanDelete(FinOperation, Context));
         }
     }
 }
